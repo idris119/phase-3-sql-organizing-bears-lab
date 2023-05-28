412a68c6fd1b492af485717b84f859ef80cbb0a4
joseph <josephmacharia575@gmail.com>
2023-05-28 16:13:33 +0300

    DONE

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES
+("Mr.Chocolate",10,"M","black","calm","yes");
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES
+("Rowdy",7,"F","black","calm","no");
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES
+("Tabitha",8,"F","brown","calm","no");
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES
+("Sergeant Brown",15,"M","brown","violent","no");
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES
+("Melissa",5,"F","white","calm","yes");
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES
+("Grinch",13,"M","Green","cheeky","yes");
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES
+("Wendy",6,"F","black","Rogue","yes");
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES
+(NULL,20,"M","black","calm","yes");
